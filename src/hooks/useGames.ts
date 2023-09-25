0e9dfde81bbc191f4307e090f3215959627aed34
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-09-25 19:39:15 +0330

    Building game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
-import { Container } from "@chakra-ui/react";
 import { CanceledError } from "axios";
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 interface FetchGamesResponse {
   count: number;
