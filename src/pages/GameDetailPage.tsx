2404f2da58673645e34dcad2a49a4b709994622c
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-10-07 01:20:25 +0330

    Build game trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -2,11 +2,11 @@ import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
+import GameTrailer from "../components/GameTrailer";
 import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
-
   const { data: game, error, isLoading } = useGame(slug!);
 
   if (isLoading) return <Spinner />;
@@ -18,6 +18,7 @@ const GameDetailPage = () => {
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 };
