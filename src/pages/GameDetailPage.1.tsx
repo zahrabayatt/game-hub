9df16c5013c112d331ca9e890d2c98ad03cc7a1c
Zahra Bayat <zahra.bayat13799@gmail.com>
2023-10-07 00:34:19 +0330

    Build game attributes

---
+++ src/pages/GameDetailPage.1.tsx
@@ -0,0 +1,37 @@
+import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import ExpandableText from "../components/ExpandableText";
+import useGame from "../hooks/useGame";
+import DefinitionItem from "../components/DefinitionItem";
+import CriticScore from "../components/CriticScore";
+
+export const GameDetailPage = () => {
+  const { slug } = useParams();
+
+  const { data: game, error, isLoading } = useGame(slug!);
+
+  if (isLoading) return <Spinner />;
+
+  if (error || !game) throw error;
+
+  return (
+    <>
+      <Heading>{game.name}</Heading>
+      <ExpandableText>{game.description_raw}</ExpandableText>
+      <DefinitionItem term="Platforms">
+        {game.parent_platforms?.map(({ platform }) => (
+          <Text key={platform.id}>{platform.name}</Text>
+        ))}
+      </DefinitionItem>
+      <DefinitionItem term="Metascore">
+        <CriticScore score={game.metacritic} />
+      </DefinitionItem>
+      <DefinitionItem term="Genres">
+        {game.genres.map((g) => (
+          <Text key={g.id}>{g.name}</Text>
+        ))}
+      </DefinitionItem>
+      <DefinitionItem term="Publishers">{game.publishers.map}</DefinitionItem>
+    </>
+  );
+};
